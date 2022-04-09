a8351e3f9f6fa9e94c62541dead3de85f42d0d91
Elizabeth Cano <ely.cano2033@gmail.com>
2022-04-09 17:50:58 -0400

    make it better

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PW,
     {
-      host: "host",
+      host: "127.0.0.1",
       dialect: "mysql",
       port: 3306,
     }
